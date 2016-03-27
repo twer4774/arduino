35bbc858307912547cf81cb323ef585f8773d736
twer4774 <twer4774@gmail.com>
2016-03-27 11:30:31 +0900

    서보모터
    서보모터제어

--- servo.angle/servo.angle.ino
+++ servo.angle/servo.angle.ino
@@ -18,7 +18,7 @@ void loop(){
       myservo.write(angle); //현재 각도값에서 +5도로 이동
     }
     else if(recvAngle == 10){ //받은 각도값이 10일때
-      angle = angle +o recvAngle;
+      angle = angle +recvAngle;
       myservo.write(angle); //현재 각도값에서 -10도로 이동
    
     }
